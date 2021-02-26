7d70018a20da6ffa1f3c5118ec0303306020a7fc
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-02-26 16:44:29 +0200

    added: test 02

--- src/app/test-02.ts
+++ src/app/test-02.ts
@@ -1,25 +1,27 @@
+import { FormsModule } from '@angular/forms';
 /**
  * Update the following components to meet the requirements : 
  * * Bind [field] of [textfield] component to its text input
  * * Pass value of [field] from [textfield] component to [title] property of component [ng-app]
  */
-import { Component, NgModule  } from '@angular/core';
+import { Component, Input, NgModule  } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector : 'textfield',
-    template : '<input type="text" value="" />'
+    template : '<input type="text" [(value)]="field" readonly/>'
 })
 export class TextField {
-    field = "";
+    @Input() field = "";
 }
 
 @Component({
     selector : 'child-component',
-    template : `<h2>Title:<h2><br/><textfield></textfield>`
+    template : `<h2>Title:<h2><br/><textfield [(field)]="field"></textfield>`
 })
 export class ChildComponent {
+    @Input() field = "";
 
 }
 
@@ -27,18 +29,20 @@ export class ChildComponent {
 @Component({
     selector : 'ng-app',
     template : `<div>
-                    <child-component></child-component> <br/>
+                    <child-component [(field)]="title"></child-component> <br/>
                     Title is {{title}}
                 </div>`
 })
 export class Test02Component {
 
-    title:string = "";
+    title:string = "Hello from parent component";
+    // also can deal with template refrence @ViewChild child.textfield.field ;)
 }
 
 @NgModule({
     imports : [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path : "",
